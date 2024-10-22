f3395f3ec0b4fe11819e35a796d2dcb74241b977
Danila <danila.blizniuk@gmail.com>
2024-10-22 12:15:47 +0300

    Added creating binary_tree

---
+++ 08/create_tree.cpp
@@ -0,0 +1,66 @@
+#include <iostream>
+#include <fstream>
+#include <set>
+
+using namespace std;
+
+struct Node 
+{
+    int key;
+    Node* right;
+    Node* left;
+    
+    Node(int value) : key(value), right(nullptr), left(nullptr) {}
+};
+
+Node* insert(Node* root, int key);
+void preOrder(Node* root, ofstream &output);
+
+int main() 
+{
+    ifstream input("input.txt");
+    ofstream output("output.txt");
+
+    Node* root = nullptr;
+    int key;
+
+    while (input >> key) 
+    {
+        root = insert(root, key);
+    }
+
+    preOrder(root, output);
+
+    input.close();
+    output.close();
+
+    return 0;
+}
+
+Node* insert(Node* root, int key) 
+{
+    if (root == nullptr) 
+    {
+        return new Node(key);
+    }
+    else if (key < root->key) 
+    {
+        root->left = insert(root->left, key);
+    } 
+    else if (key > root->key) 
+    {
+        root->right = insert(root->right, key);
+    }
+    return root;
+}
+
+void preOrder(Node* root, ofstream &output) 
+{
+    if (root != nullptr) 
+    {
+        output << root->key << "\n";
+        preOrder(root->left, output);
+        preOrder(root->right, output);
+    }
+}
+
